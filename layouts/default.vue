7613b418c23d047af8aa433566a634ae7ef90fc3
Kevinsorensen523 <94046712+Kevinsorensen523@users.noreply.github.com>
2023-07-21 21:34:31 +0700

    up

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,8 @@
     <div>
         <Navbar />
         <slot />
-<<<<<<< HEAD
         <Testimoni />
-=======
         <TestimoniTestimonis/>
->>>>>>> 2881a58232b9a83e4a666caa3854e2df5c82f904
         <Footer />
     </div>
 </template>
